b0c49c4eab4c96f66d54cc2758b71a40ceed57c0
xvolume <avolume@protonmail.com>
2019-12-19 02:09:54 +0300

    Added secret key

--- dj_forms/dj_forms/settings.py
+++ dj_forms/dj_forms/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import secret
-SECRET_KEY = secret.secret_key
+SECRET_KEY = 'x_p-4bm(7(l7@+@t-@)kr0ca2&(=secret.pyn^lk1^x=j^=#s+w_^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
